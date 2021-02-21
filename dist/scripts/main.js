e5316569b7aadb6e1b72b89c649c571d7bc0979e
julshotal <jgh6364@g.rit.edu>
2021-02-20 22:52:23 -0500

    auto prompts & next prompt button

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -32,30 +32,3 @@ let app = new Vue({
         }
     }
 });
-
-//https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
-const parseJSON = (file, callback) => {
-    const rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-};
-
-//usage:
-const generatePrompt = () => {
-    parseJSON("prompts.json", (text) => {
-        const prompts = JSON.parse(text);
-        const randomPromptType = Math.floor(Math.random()* (3 - 0) + 0);
-        const randomPrompt = Math.floor(Math.random()* (5 - 0) + 0);
-    
-        const promptName = prompts[randomPromptType].name;
-        const promptBody = prompts[randomPromptType].prompts[randomPrompt];
-    });
-};
-
-
