82d2c643a4346f5201259950cb9709784f93999a
Jin Jin Heipler <heipler.jj@gmail.com>
2021-02-20 20:23:19 -0500

    Used XML request for api call

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -9,31 +9,26 @@ let app = new Vue({
     },
     methods: {
         check() {
-            fetch("https://grammarbot.p.rapidapi.com/check", {
-                    "method": "POST",
-                    "headers": {
-                        "content-type": "application/x-www-form-urlencoded",
-                        "x-rapidapi-key": this.xRapidapiKey,
-                        "x-rapidapi-host": this.xRapidapiHost
-                    },
-                    "body": {
-                        "text": this.test,
-                        "language": "en-US"
-                    }
-                })
-                .then(response => {
-                    if (!response.ok) {
-                        throw Error(
-                            `ERROR: ${response.statusText}`
-                        );
-                    }
-                    return response.json();
-                })
-                .then(json => {
-                    console.log(json);
-                    // this.resText = json.matches.message;
-                    // console.log(json.matches.message);
-                })
+            
+            const data = "text=Susan%20go%20to%20the%20store%20everyday&language=en-US";
+
+            const xhr = new XMLHttpRequest();
+            xhr.withCredentials = true;
+
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === this.DONE) {
+                    console.log(this.responseText);
+                }
+            });
+
+            xhr.open("POST", "https://grammarbot.p.rapidapi.com/check");
+            xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+            xhr.setRequestHeader("x-rapidapi-key", "b9f3e8d36dmsh79f02cf8bc2c270p12627fjsn24bc425bc668");
+            xhr.setRequestHeader("x-rapidapi-host", "grammarbot.p.rapidapi.com");
+
+            xhr.send(data);
+
+            console.log(data);
         }
     }
 })
