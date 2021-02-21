430f4d141c3d8d542734aae9bbc5659ffbeea1ec
Jin Jin Heipler <heipler.jj@gmail.com>
2021-02-21 04:34:43 -0500

    Removed Vue

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,75 +1,65 @@
-let app = new Vue({
-    el: '#header',
-    data: {
-        xRapidapiKey: 'b9f3e8d36dmsh79f02cf8bc2c270p12627fjsn24bc425bc668',
-        xRapidapiHost: 'grammarbot.p.rapidapi.com',
-        sentenceInput: "",
-        resText: "",
-    },
-    methods: {
 
-        check() {
+const check = () => {
 
-            let newInput = this.sentenceInput.replaceAll(" ", "%20");
-            let data = `text=${newInput}`
-            const input = this.sentenceInput;
+    let xRapidapiKey = 'b9f3e8d36dmsh79f02cf8bc2c270p12627fjsn24bc425bc668';
+    let xRapidapiHost = 'grammarbot.p.rapidapi.com';
+    let newInput = document.querySelector('#promptAnswer').value.replaceAll(" ", "%20");
+    let data = `text=${newInput}`
 
-            const xhr = new XMLHttpRequest();
-            xhr.withCredentials = true;
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-            xhr.addEventListener("readystatechange", function () {
+    xhr.addEventListener("readystatechange", function () {
 
-                if (this.readyState === 4 && this.status === 200) {
-                    let res = JSON.parse(this.responseText);
+        if (this.readyState === 4 && this.status === 200) {
+            let res = JSON.parse(this.responseText);
 
-                    //console.log(res);
-                    res.matches.forEach(el => {
-                        const toastDiv = document.createElement('div');
-                        const header = document.createElement('div');
-                        const body = document.createElement('div');
-                        const span = document.createElement('span');
-                        const close = document.createElement('button');
-                        const errorMsg = document.createElement('p');
+            //console.log(res);
+            res.matches.forEach(el => {
+                const toastDiv = document.createElement('div');
+                const header = document.createElement('div');
+                const body = document.createElement('div');
+                const span = document.createElement('span');
+                const close = document.createElement('button');
+                const errorMsg = document.createElement('p');
 
-                        toastDiv.className = "toast";
-                        toastDiv.setAttribute('data-autohide', 'false');
+                toastDiv.className = "toast";
+                toastDiv.setAttribute('data-autohide', 'false');
 
-                        header.className = "toast-header";
-                        body.className = "toast-body";
-                        close.className = "ml-2 mb-1 close";
-                        close.setAttribute('data-dismiss', 'toast');
-                        close.type = "button";
-                        span.setAttribute("aria-hidden", "true");
-                        span.innerHTML = "&times;";
-                        errorMsg.innerHTML =  el.message;
+                header.className = "toast-header";
+                body.className = "toast-body";
+                close.className = "ml-2 mb-1 close";
+                close.setAttribute('data-dismiss', 'toast');
+                close.type = "button";
+                span.setAttribute("aria-hidden", "true");
+                span.innerHTML = "&times;";
+                errorMsg.innerHTML =  el.message;
 
-                        close.appendChild(span);
-                        header.appendChild(close);
-                        body.appendChild(errorMsg);
-                        toastDiv.appendChild(header);
-                        toastDiv.appendChild(body);
+                close.appendChild(span);
+                header.appendChild(close);
+                body.appendChild(errorMsg);
+                toastDiv.appendChild(header);
+                toastDiv.appendChild(body);
 
-                        document.querySelector('#toastContainment').appendChild(toastDiv);
-                    }); //Getting the message
+                document.querySelector('#toastContainment').appendChild(toastDiv);
+            }); //Getting the message
 
-                    if(res.matches.length === 0) {
-                        document.querySelector('#promptAnswer').value = "";
-                        document.querySelector('#correct').style.display = "block";
-                        $("#correct").delay(1000).fadeOut();
-                        generatePrompt();
-                    }
-                    
-                    $('.toast').toast('show');
-                }
-            });
+            if(res.matches.length === 0) {
+                document.querySelector('#promptAnswer').value = "";
+                document.querySelector('#correct').style.display = "block";
+                $("#correct").delay(1000).fadeOut();
+                generatePrompt();
+            }
+            
+            $('.toast').toast('show');
+        }
+    });
 
-            xhr.open("POST", "https://grammarbot.p.rapidapi.com/check");
-            xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-            xhr.setRequestHeader("x-rapidapi-key", `${this.xRapidapiKey}`);
-            xhr.setRequestHeader("x-rapidapi-host", `${this.xRapidapiHost}`);
+    xhr.open("POST", "https://grammarbot.p.rapidapi.com/check");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("x-rapidapi-key", `${xRapidapiKey}`);
+    xhr.setRequestHeader("x-rapidapi-host", `${xRapidapiHost}`);
 
-            xhr.send(data);
+    xhr.send(data);
 
-        },
-    }
-});
+}
